351bbb646cf5fe3819eae143318714b7f8345a77
nachomieres@gmail.com <Campoamor>
2016-05-26 12:58:07 +0200

    RC2 Gestion de permisos android 6.0

--- app/src/main/java/com/android/nacho/dondeando/MainActivity.java
+++ app/src/main/java/com/android/nacho/dondeando/MainActivity.java
@@ -1,11 +1,15 @@
 package com.android.nacho.dondeando;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -71,8 +75,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fab.setImageResource(R.drawable.ic_pausa);
         Log.i("DIALOGO", "onFinishEditDialog: OK ");
         txtNombre.setText("Guardando ruta "+ inputText);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
+        }
         Intent intent = new Intent(this, CapturaCoords.class);
         intent.putExtra("nombre", inputText);
         startService(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        switch (requestCode)
+        {
+            case 1:
+            {
+                //Si la petición es cancelada, el resultado estará vacío.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    Log.i("PERMISO", "onRequestPermissionsResult: CONCEDIDO");
+
+                } else
+                {
+                    //Permiso denegado. Desactivar la funcionalidad que dependía de dicho permiso.
+                }
+                return;
+            }
+
+            // A continuación, se expondrían otras posibilidades de petición de permisos.
+        }
     }
 }
